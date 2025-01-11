883dff6d5cc83f65e3bf98d95c2a4d6b7b8f958c
Rishav Kumar <35143365+jharishav@users.noreply.github.com>
2025-01-11 12:02:46 +0530

    Add files via upload

---
+++ carry_select adder.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11.01.2025 08:35:07
+// Design Name: 
+// Module Name: carry_select adder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module carry_select adder(
+
+    );
+endmodule
